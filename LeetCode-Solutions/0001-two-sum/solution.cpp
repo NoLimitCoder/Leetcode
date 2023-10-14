fd829cdd5bcf042cabfe5ca56ab4a80a1086cec9
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2023-10-14 22:22:50 +0000

    [LeetCode Sync] Runtime - 11 ms (64.27%), Memory - 11.2 MB (99.98%)

--- LeetCode-Solutions/0001-two-sum/solution.cpp
+++ LeetCode-Solutions/0001-two-sum/solution.cpp
@@ -1,24 +1,13 @@
-#define deb(x) cerr << #x << ": " << x << '\n'
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = (int)nums.size();
-        int i = 0, j = n-1;
-        vector<int> ans;
-        vector<pair<int,int>> nums2(n);
-        for(int idx = 0; idx < n; ++idx){
-            nums2[idx] = make_pair(nums[idx], idx);
+        unordered_map<int,int> mp;
+        for(int i = 0; i< nums.size(); ++i){
+            if( mp.find(target - nums[i]) != mp.end()){
+                return {i, mp[target-nums[i]]};
+            }
+            mp[nums[i]]=i;
         }
-        sort(nums2.begin(), nums2.end());
-        while(i < j){
-            int sum = nums2[i].first + nums2[j].first;
-            if(sum> target)
-                {j--;}
-            else if (sum < target)
-                {i++;}
-            else
-                {ans.push_back(nums2[i].second);ans.push_back(nums2[j].second); break;}
-        }
-        return ans;
+        return {};
     }
 };
