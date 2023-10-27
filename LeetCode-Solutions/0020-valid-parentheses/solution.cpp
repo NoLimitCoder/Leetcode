40a2df2881af9808e2f107a4fc464f910890dcd0
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2023-10-27 23:06:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.7 MB (100.00%)

--- LeetCode-Solutions/0020-valid-parentheses/solution.cpp
+++ LeetCode-Solutions/0020-valid-parentheses/solution.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-
-bool check(char c, char t){
-    if(c == '{') c = '}';
-    if(c == '[') c = ']';
-    if(c == '(') c = ')';
-    return c == t;
-}
-
-
-
     bool isValid(string s) {
-        stack<char> stk;
-        for(int i = 0; i<s.size();++i){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-                stk.push(s[i]);
+     stack<char>stk;
+     for(int i = 0; i< s.size(); ++i){
+         if(s[i] == '{' || s[i] == '(' || s[i] == '['){
+             stk.push(s[i]);
+             continue;
+         }
+         else{
+             if(stk.empty()){return false;}
+             char top = stk.top();
+             if(top == '{' && s[i] == '}'){
+                 stk.pop();
+             }
+             else if(top == '(' && s[i] == ')'){
+                 stk.pop();
             }
-            else{
-                if(stk.size()==0){return false;}
-                if(check(stk.top(),s[i])){
-                    //cout<< stk.top() << ' ' << s[i] << '\n';
-                    stk.pop(); continue;
-                }
-                else{return false;}
+            else if(top == '[' && s[i] == ']'){
+                stk.pop();
+            }
+            else {
+                return false;
             }
         }
-        return stk.size()==0;
+     }
+    return stk.empty();
     }
 };
