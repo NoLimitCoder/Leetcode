e6251ab9c6039d0e9f181589cf54d696c1008d3a
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2024-06-21 08:29:43 +0000

    [LeetCode Sync] Runtime - 55 ms (35.31%), Memory - 38.9 MB (21.60%)

--- LeetCode-Solutions/0720-longest-word-in-dictionary/solution.cpp
+++ LeetCode-Solutions/0720-longest-word-in-dictionary/solution.cpp
@@ -1,36 +1,54 @@
 class Solution {
 public:
 struct vertex{
-    char l; bool end; vector<vertex*>child;
-    vertex(char c): l(c), end(0) {child.assign(26,NULL);} 
+  char l; bool end; vector<vertex*>child;
+  vertex(char c): l(c), end(0) {child.assign(26,NULL);}
 };
 
 struct Trie{
     vertex* root;
-    Trie(){root = new vertex('!');}
+    Trie(){root = new vertex('l');}
+
     void insert(string word){
-        vertex* cur = root;
-        for(char c : word){
-            if(cur->child[c-'a'] == NULL)
-                cur->child[c-'a'] = new vertex(c);
-            cur = cur->child[c-'a'];
-        }
-        cur->end = 1;
+      vertex* cur = root;
+      for(char c : word){
+        if(cur->child[c-'a'] == NULL)
+          cur->child[c-'a'] = new vertex(c);
+        cur = cur->child[c-'a'];
+      }
+      cur->end = 1;
+    }
+
+    bool search(string word){
+      vertex* cur = root;
+      for(char c : word){
+        if(cur->child[c-'a'] == NULL)
+          return false;
+        cur = cur->child[c-'a'];
+      }
+      return cur->end;
+    }
+
+    bool startsWith(string word){
+      vertex* cur = root;
+      for(char c : word){
+        if(cur->child[c-'a'] == NULL)
+          return false;
+        cur = cur->child[c-'a'];
+      }
+      return 1;
     }
 
     bool allExist(string word){
-        vertex* cur = root;
-        for(char c : word){
-            if(cur->child[c-'a'] == NULL || !cur->child[c-'a']->end)
-                return false;
-            cur = cur->child[c-'a'];
-        }
-        return 1;
+      vertex* cur = root;
+      for(char c : word){
+        if(cur->child[c-'a'] == NULL || !cur->child[c-'a']->end)
+          return false;
+        cur = cur->child[c-'a'];
+      }
+      return 1;
     }
-
 };
-
-
 
     string longestWord(vector<string>& words) {
         int len = 0; string ans;
