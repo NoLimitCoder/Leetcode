eb03ade46360bfd8fd9e465fa9f2c45f671f2a99
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2024-06-26 08:42:09 +0000

    [LeetCode Sync] Runtime - 83 ms (71.19%), Memory - 113.2 MB (43.45%)

--- LeetCode-Solutions/1037-minimum-number-of-k-consecutive-bit-flips/solution.cpp
+++ LeetCode-Solutions/1037-minimum-number-of-k-consecutive-bit-flips/solution.cpp
@@ -1,10 +1,18 @@
+#pragma GCC optimize("O3,unroll-loops")
+
 class Solution {
 public:
     int minKBitFlips(vector<int>& A, int k) {
-        int cur = 0, ans = 0, n = A.size();
-        for (int i = 0; i < n; ++i) {
-            if (i >= k && A[i - k] > 1) {cur--; A[i - k] -= 2;}
-            if (cur % 2 == A[i]) { if (i + k > n) return -1; A[i] += 2; cur++; ans++;}
+        int ans = 0, n = A.size();
+        vector<int> difference_array(n+1);
+        int sweep_line_flip = 0;
+        for(int i = 0; i<n; ++i){
+            sweep_line_flip += difference_array[i]; sweep_line_flip %= 2;
+            if(A[i] ^ sweep_line_flip == 0){
+                if(i+k-1 >= n) return -1;
+                difference_array[i] += 1; sweep_line_flip += 1; ans++;
+                difference_array[i+k] -= 1;
+            }
         }
         return ans;
     }
