482ff417307957ee2f79f31f9c0c355966499446
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2024-06-30 04:10:39 +0000

    [LeetCode Sync] Runtime - 119 ms (40.28%), Memory - 94.4 MB (39.12%)

---
+++ LeetCode-Solutions/3490-find-the-maximum-length-of-valid-subsequence-i/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& A) {
+        //1 force even
+        int n = A.size();
+        int even = 0, odd = 0;
+        for(int i = 0; i<n; ++i){
+            if(A[i]&1)odd++;
+            else even++;
+        }
+        //case 3
+        int ans = 0;
+        bool flag = 0;
+        int count = 0;
+        for(int i = 0; i<A.size(); ++i){
+            if(flag) { if(A[i]&1) {count++; flag = !flag;} }
+            else{ if(A[i]%2 == 0 ) {count++; flag = !flag;} }
+        }
+        ans = max(ans,count);
+        flag = 0;
+        count = 0;
+        for(int i = 0; i<A.size(); ++i){
+            if(!flag) { if(A[i]&1) {count++; flag = !flag;}}
+            else{  if(A[i]%2 == 0 ) {count++; flag = !flag;} }
+            
+        }
+        ans = max(ans,count);
+     return max({even, odd, ans});
+    }
+};
