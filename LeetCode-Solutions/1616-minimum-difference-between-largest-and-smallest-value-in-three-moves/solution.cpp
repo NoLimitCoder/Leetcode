942bf110e2aba79748fac293532b14689479b3b1
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2024-07-05 02:46:47 +0000

    [LeetCode Sync] Runtime - 29 ms (98.87%), Memory - 38.8 MB (11.71%)

--- LeetCode-Solutions/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/solution.cpp
+++ LeetCode-Solutions/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/solution.cpp
@@ -1,5 +1,4 @@
-#define deb(x) cout << #x << ": " << x << '\n'
-#define debl(x) cout << #x << ": " << x << ' '
+
 class Solution {
 public:
     int minDifference(vector<int>& A) {
@@ -13,3 +12,9 @@ public:
         return ans;
     }
 };
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
