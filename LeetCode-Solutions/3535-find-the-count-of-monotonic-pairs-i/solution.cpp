9112c99896fa438b83efb0a04f3897d42fea6674
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2024-08-12 23:36:17 +0000

    [LeetCode Sync] Runtime - 80 ms (95.24%), Memory - 78.5 MB (42.82%)

--- LeetCode-Solutions/3535-find-the-count-of-monotonic-pairs-i/solution.cpp
+++ LeetCode-Solutions/3535-find-the-count-of-monotonic-pairs-i/solution.cpp
@@ -11,15 +11,20 @@ public:
         int n = A.size(), m = *max_element(A.begin(), A.end());
         vector<vector<int>> dp(n+1, vector<int>(m+1));
         for(int i = 0; i<=A[0]; ++i) dp[0][i] = 1;
-
+        vector<int> P(m+5);
         for(int i = 1; i<n; i++){
-            for(int j = A[i]; j >= 0; j--){          //A[i]          -> j -> arr1
-                int k = A[i]-j;                      //A[i]  - j     -> k -> arr2
-                for(int x = 0; x <= A[i-1]; x++){   //A[i-1]         -> x -> arr1
-                    int y = A[i-1]-x;               //A[i-1] - x     -> y -> arr2
-                    if(x <= j && y >= k) dp[i][j] += dp[i-1][x];
-                    dp[i][j] %= MOD;
-                }
+            vector<int> P(m+5,0);
+            for(int l = 0; l <= A[i]; ++l) { P[l]+=dp[i-1][l]; if(l) P[l] += P[l-1]; P[l]%=MOD;}
+            for(int j = A[i]; j >= 0; j--){          
+                int k = A[i]-j-1;
+                int r = min(j+1, A[i-1]-k);
+               // deb(r);
+                //for(int x = 0; x < r; x++){    //x < j+1 && x < A[i-1]-k;
+                    //dp[i][j] += dp[i-1][x]; dp[i][j] %= MOD;
+                    if(r>0)dp[i][j] += P[r-1]; dp[i][j] %= MOD;
+                    
+                  //  debl(x); cout << "-> " << dp[i-1][x] << ' '; 
+                //} //deb(P[r-1]); //cout << '\n'; 
             }
         }
         int ans = 0;
