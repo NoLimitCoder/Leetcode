4065a4d28a5ae257eb92c4e17e0ec15322379811
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-02-13 07:14:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (31.89%)

--- LeetCode-Solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
+++ LeetCode-Solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        auto it = haystack.find(needle);
-        if( it != string::npos){
-            return it;
+    int strStr(string s, string p) {
+        int m = p.size(), n = s.size(); vector<int> LPS(m,0);
+        //kmp
+        for(int i = 1, j = 0; i<m;){ p[i]==p[j]? LPS[i++] = ++j : j? j = LPS[j-1] : ++i; }
+        //search
+        for(int i = 0, j = 0; i<n;){ s[i]==p[j]? i++, ++j : j? j = LPS[j-1] : ++i; 
+            //if(j==m) {cout << i-m << '\n'; j = LPS[j-1]; }
+            if(j==m) return i-m;
         }
         return -1;
-        
     }
 };
