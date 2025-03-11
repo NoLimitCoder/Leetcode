24c578d43b23e40a3f7a30deb9c853f3a37edbed
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-03-11 19:28:52 +0000

    [LeetCode Sync] Runtime - 941 ms (5.41%), Memory - 197.4 MB (5.25%)

--- LeetCode-Solutions/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/solution.cpp
+++ LeetCode-Solutions/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/solution.cpp
@@ -1,4 +1,3 @@
-#define deb(x) cout << #x << ": " << x << '\n'
 typedef long long ll;
 class Solution {
 public:
@@ -8,47 +7,30 @@ bool valid(vector<int> &mp, int k){
 }
 
     long long countOfSubstrings(string s, int k) {
-        set<ll> st;
-        vector<int> mp(27);
-        ll n = s.size();
+        set<ll> st; vector<int> mp(27);
+        ll n = s.size(), i = 0, j = 0, ans = 0;
         st.insert(n);
-        for(int i = 0; i<n; ++i){
-            if(s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u')
-            st.insert(i);
-        }
-       //for(auto it : st) cout << it << ' ';
-        //cout << '\n';
-        ll i = 0, j = 0, ans = 0;
+        for(int i = 0; i<n; ++i)
+            if(s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u') st.insert(i);
+        
         while(j < n){
-            if(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
-                mp[s[j]-'a']++;
-            else 
-                mp[26]++;
+            if(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u') mp[s[j]-'a']++;
+            else mp[26]++;
             while(mp[26] > k){
-                if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
-                    mp[s[i]-'a']--;
-                else 
-                    mp[26]--;
+                if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') mp[s[i]-'a']--;
+                else mp[26]--;
                 ++i;
             }
-            //cout << i << ' ' << j << '\n';
             while(i<n && valid(mp,k)){
                 ll pos = *st.upper_bound(j);
-                //deb(pos);
                 ans += (pos-j);
-                if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
-                    mp[s[i]-'a']--;
-                else 
-                    mp[26]--;
+                if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') mp[s[i]-'a']--;
+                else mp[26]--;
                 ++i;
-                //j = max(j,i);
             }
-                ++j;
+            ++j;
         }
         return ans;
     }
-
 };
 
-//"iqeaouqi"
-//k = 2
