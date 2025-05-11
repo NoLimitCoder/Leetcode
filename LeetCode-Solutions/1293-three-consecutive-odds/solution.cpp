23f2057a88a5d11c1359f03dce12b356d0e09cc7
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-05-11 01:58:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.8 MB (85.50%)

--- LeetCode-Solutions/1293-three-consecutive-odds/solution.cpp
+++ LeetCode-Solutions/1293-three-consecutive-odds/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& A) {
-        for(int i = 2; i<A.size(); ++i){ if( (A[i]%2==1) && (A[i-1]&1) && (A[i-2]&1) ) return true; }
+        for(int i = 2; i<A.size(); ++i){ if( (A[i]%2==1) && (A[i-1]%2==1) && (A[i-2]%2==1) ) return true; }
         return false;
     }
 };
