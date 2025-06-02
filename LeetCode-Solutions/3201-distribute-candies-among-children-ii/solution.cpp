9601ef2a2eb65e20af594aec17e5dc6302db19d8
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-06-02 00:27:14 +0000

    [LeetCode Sync] Runtime - 5 ms (68.70%), Memory - 9 MB (51.85%)

---
+++ LeetCode-Solutions/3201-distribute-candies-among-children-ii/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long gauss(int x) {
+        if (x < 0) return 0;
+        return (long)x * (x - 1) / 2;
+    }
+
+    long long distributeCandies(int n, int limit) {
+        return gauss(n + 2) - 3 * gauss(n - limit + 1) +
+               3 * gauss(n - (limit + 1) * 2 + 2) - gauss(n - 3 * (limit + 1) + 2);
+    }
+};
