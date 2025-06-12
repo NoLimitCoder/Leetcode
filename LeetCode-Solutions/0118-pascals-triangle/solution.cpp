ac948195813483f11dcae4c6fcaf18f38be1dd29
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-06-12 15:29:54 +0000

    [LeetCode Sync] Runtime - 3 ms (8.07%), Memory - 9.6 MB (62.91%)

---
+++ LeetCode-Solutions/0118-pascals-triangle/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int x) {
+        vector<vector<int>>dp(x);
+        //base cases
+        for(int i = 0; i<x; ++i){
+            dp[i] = vector<int>(i+1);
+            dp[i][0] = 1; dp[i][i] = 1;
+        }
+        //dp
+        for(int i = 1; i<x; ++i){
+            for(int j = 1; j<=i; ++j){
+                if(i+1 < x) dp[i+1][j] = dp[i][j] + dp[i][j-1];
+            }
+        }
+        return dp;
+    }
+};
