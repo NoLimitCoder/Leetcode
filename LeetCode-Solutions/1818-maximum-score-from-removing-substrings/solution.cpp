5fc9d80bfa7f451f542e2bd2027baf2463d53537
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-07-23 02:23:43 +0000

    [LeetCode Sync] Runtime - 31 ms (66.85%), Memory - 17.4 MB (98.39%)

--- LeetCode-Solutions/1818-maximum-score-from-removing-substrings/solution.cpp
+++ LeetCode-Solutions/1818-maximum-score-from-removing-substrings/solution.cpp
@@ -1,51 +1,32 @@
-auto init = []() { ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); return 'c'; }();
-
 class Solution {
 public:
-    int maximumGain(string s, int x, int y) {
-        int n = s.size(), ans = 0;
-        string stk;
-        for(int i = 0; i<n; ++i){
-           if(stk.size() == 0) {stk+=s[i]; continue;}
-            int top = stk.size()-1;
-            if(x > y){
-                if(stk[top] == 'a' && s[i] == 'b'){
-                    ans += x;
-                    stk.pop_back();
-                    continue;
-                }
-            }
-            else{
-                if(stk[top] == 'b' && s[i] == 'a'){
-                    ans += y;
-                    stk.pop_back();
-                    continue;
-                }
-            }
+    static inline int f(string& s, char* X, int x){
+        int n=s.size(), sz=0;
+        for(int i=0; i<n; i++){
+            s[sz++]=s[i];
+            if (sz>=2 && s[sz-2]==X[0] && s[sz-1]==X[1]) 
+                sz-=2;
+        }
+        s.resize(sz);
+        return (n-sz)/2*x;
+    }
 
-        stk+=s[i];
+    static int maximumGain(string& s, int x, int y) {
+        char X[3]="ab", Y[3]="ba";
+        if (x<y){//may assume x>=y
+            swap(X, Y);
+            swap(x, y);
         }
-        string s2 = stk;
-        stk = "";
-        for(int i = 0; i<s2.size(); ++i){
-            if(stk.size() == 0) {stk+=s2[i]; continue;}
-            int top = stk.size()-1;
-            if(x < y){
-                if(stk[top] == 'a' && s2[i] == 'b'){
-                    ans += x;
-                    stk.pop_back();
-                    continue;
-                }
-            }
-            else{
-                if(stk[top] == 'b' && s2[i] == 'a'){
-                    ans += y;
-                    stk.pop_back();
-                    continue;
-                }
-            }
-        stk+=s2[i];
-        }
-        return ans;
+        
+        return f(s, X, x)+f(s, Y, y);
     }
 };
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
