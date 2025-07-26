d269a5e99e89ebdf26e226bccf8971fbf5b9d8c1
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-07-26 20:06:24 +0000

    [LeetCode Sync] Runtime - 386 ms (7.69%), Memory - 216.2 MB (5.22%)

--- LeetCode-Solutions/0239-sliding-window-maximum/solution.cpp
+++ LeetCode-Solutions/0239-sliding-window-maximum/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& A, int k) {
-        int n = A.size(); multiset<int> st; vector<int> ans;
-        //setup the window
-        for(int i = 0; i<k; ++i){st.insert(A[i]);}
-        //add first element to window
-        ans.push_back(*st.rbegin());
-        for(int i = k; i<n; ++i){
-            st.erase(st.find(A[i-k]));
-            st.insert(A[i]);
+        multiset<int> st; int n = A.size();
+        for(int i = 0; i<k; ++i) st.insert(A[i]);
+        int mx = *st.rbegin(), l = 0;
+        vector<int> ans; ans.push_back(mx);
+        for(int r = k; r<n; ++r){
+            st.insert(A[r]);
+            st.erase(st.find(A[l++]));
             ans.push_back(*st.rbegin());
         }
         return ans;
