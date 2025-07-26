fcc5cce85080d8a3e4636204bf535f9ddf383f5e
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-07-26 06:33:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.9 MB (35.56%)

---
+++ LeetCode-Solutions/0856-consecutive-numbers-sum/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int consecutiveNumbersSum(int n) {
+        int ans = 0;
+        for (int k = 1; k * (k - 1) < 2 * n; ++k) {
+            if ((2 * n - k * (k - 1)) % (2 * k) == 0) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
+
