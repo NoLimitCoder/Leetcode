096c99825bada8e2ea2d8245515f025e9644088d
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-07-27 20:08:51 +0000

    [LeetCode Sync] Runtime - 95 ms (16.81%), Memory - 193.8 MB (19.76%)

--- LeetCode-Solutions/2869-longest-non-decreasing-subarray-from-two-arrays/solution.cpp
+++ LeetCode-Solutions/2869-longest-non-decreasing-subarray-from-two-arrays/solution.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int maxNonDecreasingLength(vector<int>& A, vector<int>& B) {
         int n = A.size();
-        vector<vector<int>> dp(n,vector<int>(2,1));
-        //state: dp[i][0] -> at position i I'm taking A[i], dp[i][1] at i I'm taking B[i]
-        //transition, dp[i][0] = if cannot come from any A or B i.e. A[i] < A[i-1] && A[i] < B[i-1] = 1
-        //else dp[i][0] = max(dp[i-1][0], dp[i-1][1]); the ones you can come from
+        //dp[i][2] lISubarray ending at i choosing array [1] | [2] as current option
+        /*
+            transition: dp[i][0] can come from dp[i-1][0] | dp[i][1] if A[i] >= A[i-1] | A[i] >= B[i-1]
+        */
         int mx = 1;
-        for(int i = 1; i<n; ++i){
-            //dp[i][0]
-            if(A[i] >= A[i-1]) dp[i][0] = max(dp[i][0], dp[i-1][0]+1);
-            if(A[i] >= B[i-1]) dp[i][0] = max(dp[i][0], dp[i-1][1]+1);
-            //dp[i][1]
-            if(B[i] >= A[i-1]) dp[i][1] = max(dp[i][1], dp[i-1][0]+1);
-            if(B[i] >= B[i-1]) dp[i][1] = max(dp[i][1], dp[i-1][1]+1);
-
-            mx = max({mx, dp[i][0], dp[i][1]});
+        vector<vector<int>> dp(n,vector<int>(2));
+        for(int i = 0; i<n; ++i){
+            //A[i]
+            dp[i][0] = max(dp[i][0], 0 + 1);
+            if(i-1 >=0 && A[i] >= A[i-1]) dp[i][0] = max(dp[i][0], dp[i-1][0]+1);
+            if(i-1 >=0 && A[i] >= B[i-1]) dp[i][0] = max(dp[i][0], dp[i-1][1]+1);
+            //B[i]
+            dp[i][1] = max(dp[i][1], 0 + 1);
+            if(i-1 >=0 && B[i] >= A[i-1]) dp[i][1] = max(dp[i][1], dp[i-1][0]+1);
+            if(i-1 >=0 && B[i] >= B[i-1]) dp[i][1] = max(dp[i][1], dp[i-1][1]+1);
+            mx = max({mx,dp[i][0],dp[i][1]});
         }
         return mx;
     }
