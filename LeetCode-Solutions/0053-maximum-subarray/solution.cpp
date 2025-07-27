933bd39764b7a50573a5fdd4fc1c5fd0ae2acf2f
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-07-27 18:37:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 71.8 MB (16.67%)

--- LeetCode-Solutions/0053-maximum-subarray/solution.cpp
+++ LeetCode-Solutions/0053-maximum-subarray/solution.cpp
@@ -1,19 +1,16 @@
 #define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 class Solution {
 public:
-int INF = 2e9;
+typedef long long ll;
 
-
-    int maxSubArray(vector<int>& nums) {
-        fast_io();
-        //Kadane's Algo
-        int mx = -INF;
-        int sum = 0;
-        for(int i = 0; i<nums.size(); ++i){
-            if(sum < 0) sum = 0; 
-            sum += nums[i];
-            mx = max(mx,sum);
+    int maxSubArray(vector<int>& A) {
+        ll ans = A[0];
+        ll sum = A[0];
+        for(int i = 1; i<A.size(); ++i){
+            if(sum < 0) sum = 0;
+            sum += A[i];
+            ans = max(ans,sum);
         }
-        return mx;
+        return ans;
     }
 };
