df50171ea9ae973bcf5e410fc01c9128266362e4
Sebastian Dionicio <73214285+NoLimitCoder@users.noreply.github.com>
2025-07-27 20:15:37 +0000

    [LeetCode Sync] Runtime - 145 ms (58.93%), Memory - 145.3 MB (40.30%)

--- LeetCode-Solutions/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/solution.cpp
+++ LeetCode-Solutions/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/solution.cpp
@@ -1,11 +1,13 @@
 typedef long long ll;
 class Solution {
 public:
-    long long minSum(vector<int>& A, vector<int>& B) {
-        ll s1 = 0, s2 = 0, z1 = 0, z2 = 0;
-        for(ll i : A) {if(!i) z1++; s1+=i;}
-        for(ll i : B) {if(!i) z2++; s2+=i;}
-        if( (!z1 && s1+z1 < s2+z2) || (!z2 && s2+z2 < s1+z1)) return -1;
-        return max(s1+z1,s2+z2);
+    ll minSum(vector<int>& A, vector<int>& B) {
+        ll s1=0, s2=0, z1=0, z2=0;
+        for(int i : A){s1 += i; if(i==0) z1++;} 
+        for(int i : B){s2 += i; if(i==0) z2++;}
+        if( (z1 == 0 && s2+z2 > s1) || (z2 == 0 && s1+z1 > s2)) return -1;
+        else{
+            return max(s1+z1,s2+z2);
+        }
     }
 };
